de3c68caf76f3137c6c171945db89873e4f8b8a9
KajWiik <kaj.wiik@iki.fi>
2021-12-29 18:59:31 +0000

    Deploying to gh-pages from  @ a763aff9d5de70a0b3237cb786b20cd23ab479bc 🚀

---
+++ index.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.17.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 611e50d6-688d-11ec-02b9-a764d2095253
+md"""
+# Notebooks
+
+*  [Notebook](notebook.html)
+*  [My cool notebook](My cool notebook.html)
+
+
+[Duck Duck Go](https://duckduckgo.com)
+"""
+
+# ╔═╡ Cell order:
+# ╟─611e50d6-688d-11ec-02b9-a764d2095253
